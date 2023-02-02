62866327f4b0f950bd17942cf164452980ade0a0
fymorGod <mufinagamer@gmail.com>
2023-02-02 18:12:55 -0300

    feat: create form and validations :white_check_mark:

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,85 @@
+import { HisotoryList, HistoryContainer, Status } from "./styles";
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HisotoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">
+                  Concluido
+                </Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="red">
+                Interrompido
+                </Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">
+                  Concluido
+                </Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="yellow">
+                Em Andamento
+                </Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="yellow">
+                  Em Andamento
+                </Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="red">
+                  Interrompido
+                </Status>
+              </td>
+            </tr>
+
+          </tbody>
+        </table>
+      </HisotoryList>   
+    </HistoryContainer>
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,8 @@
 import { Play } from 'phosphor-react';
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
 import { 
   CountDownContainer, 
   FormContainer, 
@@ -9,12 +13,28 @@ import {
   TaskInput } from './styles';
 
 export function Home() {
+
+  const { register, handleSubmit, watch } = useForm()
+
+  function handleCreateNewCycle(data) {
+    console.log(data)
+  }
+
+  const task = watch('task');
+  const isSubmitDisabled = !task;
+
   return ( 
     <HomeContainer>
-      <form>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em </label>
-          <TaskInput id="task" list="task-suggestions" type="text" placeholder='Dê um nome para o seu projeto' />
+          <TaskInput
+          id="task" 
+          list="task-suggestions" 
+          type="text" 
+          placeholder='Dê um nome para o seu projeto'
+          {...register('task')} 
+          />
           <datalist id='task-suggestions'>
             <option value="Projeto 1" />
             <option value="Projeto 2" />
@@ -29,6 +49,7 @@ export function Home() {
             step={5} 
             min={5} 
             max={60} 
+          {...register('minutesAmount', { valueAsNumber: true})} 
           />
           <span>minutos.</span>
         </FormContainer>
@@ -41,7 +62,7 @@ export function Home() {
         <span>0</span>
       </CountDownContainer>
 
-      <StartCountdownButton type="submit">
+      <StartCountdownButton disabled={isSubmitDisabled} type="submit">
         <Play size={24} />
         Começar
       </StartCountdownButton>
